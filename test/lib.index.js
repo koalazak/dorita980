656b8cd8a45c56fe1fe6c643cf7e48baec303a72
Facu <zak.tux@gmail.com>
2016-09-28 18:14:24 -0300

    unit test basics

--- test/lib.index.js
+++ test/lib.index.js
@@ -3,9 +3,16 @@
 
 const chai = require('chai');
 const expect = chai.expect;
+const dorita980 = require('../index');
 
-describe('dorita980', () => {
-  it('TODO', () => {
-    expect('TODO').to.be.equal('TODO');
+describe('dorita980 require', () => {
+  it('should return an object with functions', () => {
+    expect(dorita980).to.be.instanceof(Object);
+    expect(dorita980).to.have.property('Cloud');
+    expect(dorita980).to.have.property('Local');
+    expect(dorita980).to.have.property('getRobotIP');
+    expect(dorita980.Cloud).to.be.instanceof(Function);
+    expect(dorita980.Local).to.be.instanceof(Function);
+    expect(dorita980.getRobotIP).to.be.instanceof(Function);
   });
 });
