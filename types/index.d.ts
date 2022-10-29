29b3e888d1f7c4c4128d63f713af384f13f3d3f2
Kolton Bloom <bloomkd46@icloud.com>
2022-10-29 15:29:52 -0700

    removed declare module 'dorita980' from index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -7,57 +7,56 @@ import { Local as LocalV3 } from './V3/local';
 
 
 
-declare module 'dorita980' {
-  export function Cloud(username: string, password: string, version: 1): CloudV1;
-  export function Cloud(username: string, password: string, version: 2): CloudV2;
-  export function Cloud(username: string, password: string, version: 3): CloudV3;
-  export function Cloud(username: string, password: string, version?: number): CloudV2 | CloudV3;
+export function Cloud(username: string, password: string, version: 1): CloudV1;
+export function Cloud(username: string, password: string, version: 2): CloudV2;
+export function Cloud(username: string, password: string, version: 3): CloudV3;
+export function Cloud(username: string, password: string, version?: number): CloudV2 | CloudV3;
 
 
-  export function Local(blid: string, password: string, ip: string, version: 1): LocalV1;
-  export function Local(blid: string, password: string, ip: string, version: 2, interval?: number): LocalV2;
-  export function Local(blid: string, password: string, ip: string, version: 3, interval?: number): LocalV3;
-  export function Local(blid: string, password: string, ip: string, version?: number, interval?: number): LocalV12 | LocalV3;
+export function Local(blid: string, password: string, ip: string, version: 1): LocalV1;
+export function Local(blid: string, password: string, ip: string, version: 2, interval?: number): LocalV2;
+export function Local(blid: string, password: string, ip: string, version: 3, interval?: number): LocalV3;
+export function Local(blid: string, password: string, ip: string, version?: number, interval?: number): LocalV12 | LocalV3;
 
-  /**
-   * If you don't known which IP address to use in {@link Local()} you can use {@link getRobotIP()} to find it. This process takes 1-2 seconds, so if you know the IP you can just use it explicity.
-   *
-   * You need UDP broadcast enabled in your network!
-   * 
-   * @example ```
-   * var dorita980 = require('dorita980');
-   *
-   * dorita980.getRobotIP((ierr, ip) => {
-   *   if (ierr) return console.log('error looking for robot IP');
-   *
-   *   var myRobotViaLocal = new dorita980.Local('MyUsernameBlid', 'MyPassword', ip);
-   *
-   *   myRobotViaLocal.getMission()
-   *     .then((mission) => {
-   *       console.log(mission);
-   *     }).catch((err) => {
-   *       console.log(err);
-   *     });
-   *   });
-   * ```
-   */
-  export function getRobotIP(callback: (err, ip: string) => void): void;
-  /**
-   * You can also use {@link discovery} method to get all the robots discovery data:
-   * 
-   * You need UDP broadcast enabled in your network!
-   * 
-   * @example ```
-   * var dorita980 = require('dorita980');
-   *
-   * dorita980.discovery((ierr, data) => {
-   *   console.log(data);
-   * });
-   * ```
-   */
-  export function discovery(callback: (err, data: DiscoveryData) => void): void;
-  export function getRobotPublicInfo(callback: (err, data: PublicInfo) => void): void;
-}
+/**
+ * If you don't known which IP address to use in {@link Local()} you can use {@link getRobotIP()} to find it. This process takes 1-2 seconds, so if you know the IP you can just use it explicity.
+ *
+ * You need UDP broadcast enabled in your network!
+ * 
+ * @example ```
+ * var dorita980 = require('dorita980');
+ *
+ * dorita980.getRobotIP((ierr, ip) => {
+ *   if (ierr) return console.log('error looking for robot IP');
+ *
+ *   var myRobotViaLocal = dorita980.Local('MyUsernameBlid', 'MyPassword', ip);
+ *
+ *   myRobotViaLocal.getMission()
+ *     .then((mission) => {
+ *       console.log(mission);
+ *     }).catch((err) => {
+ *       console.log(err);
+ *     });
+ *   });
+ * ```
+ */
+export function getRobotIP(callback: (err, ip: string) => void): void;
+/**
+ * You can also use {@link discovery} method to get all the robots discovery data:
+ * 
+ * You need UDP broadcast enabled in your network!
+ * 
+ * @example ```
+ * var dorita980 = require('dorita980');
+ *
+ * dorita980.discovery((ierr, data) => {
+ *   console.log(data);
+ * });
+ * ```
+ */
+export function discovery(callback: (err, data: DiscoveryData) => void): void;
+export function getRobotPublicInfo(callback: (err, data: PublicInfo) => void): void;
+
 interface DiscoveryData {
   ver: '2' | string,
   hostname: string,
